34efe666bddeeed85ac07892a0f3f209f409063a
Neptix <76396699+NeptuneNeptix@users.noreply.github.com>
2021-08-05 20:14:50 +0700

    Add files via upload

--- js/list.js
+++ js/list.js
@@ -1,1 +1,40 @@
-d
+const d = {
+	"list": [
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "NameData",
+					"link": "https://www.youtube.com/watch?v=fFVoY5MOvk0",
+					"percent": 100,
+					"hz": "144Hz"
+				},
+
+      ],
+			"name": "Stalemate",
+			"author": "NameData",
+			"more": "none",
+			"id": 27,
+			"pass": "Free to copy",
+			"percentToQualify": 54,
+			"verificationVid": "https://www.youtube.com/watch?v=fFVoY5MOvk0",
+/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
+/*cleared old shitty demonlist because ofc*/
+			"key": 0
+		},
+		/*=================================================================================*/
+
+
+
+
+/*only modify version with every MAJOR update (you can always check update number at https://github.com/electroflameofficial/gdshittylist/releases*/
+
+	],
+	"version": [
+		1,
+		0,
+		0
+	]
+};
+
+const list = d.list;const version = d.version;
