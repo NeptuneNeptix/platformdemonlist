1cc150900db73fbf2784ee082d23c1f306c9ba3e
Neptix <76396699+NeptuneNeptix@users.noreply.github.com>
2021-08-12 17:38:33 +0700

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,40 +1,54 @@
 const d = {
-	"list": [
-		/*=================================================================================*/
-		{
-			"vids": [
-				{
-					"user": "NameData",
-					"link": "https://www.youtube.com/watch?v=fFVoY5MOvk0",
-					"percent": 100,
-					"hz": "144Hz"
-				},
-
-      ],
-			"name": "Stalemate",
-			"author": "NameData",
-			"more": "none",
-			"id": 27,
-			"pass": "Free to copy",
-			"percentToQualify": 54,
-			"verificationVid": "https://www.youtube.com/watch?v=fFVoY5MOvk0",
-/*for legacy list add the word "legacy" behind the word key right away: "legacykey"*/
-/*cleared old shitty demonlist because ofc*/
-			"key": 0
-		},
-		/*=================================================================================*/
-
-
-
-
-/*only modify version with every MAJOR update (you can always check update number at https://github.com/electroflameofficial/gdshittylist/releases*/
-
-	],
-	"version": [
-		1,
-		0,
-		0
-	]
+  "list": [
+    /*===========*/
+    {
+      "vids": [
+        {
+          "user": "NameData",
+          "link": "https://streamable.com/tdysch",
+          "percent": 100,
+          "hz": "144hz"
+        },
+        ],
+        "name": "The Silver",
+        "author": "FrrozenJR",
+        "more": "none",
+        "id": 6,
+        "pass": "Not available",
+        "percentToQualify": "free",
+        "verificationVid": "www.youtube.com/watch?v=dHtzmlZPB4k",
+        "key": 0
+    },
+    {
+      "vids": [
+        ],
+        "name": "Wavy",
+        "author": "FrrozenJR",
+        "more": "none",
+        "id": 7,
+        "pass": 17582,
+        "percentToQualify": 69,
+        "verificationVid": "www.youtube.com/watch?v=UKnDTr1UQPM",
+        "key": 1
+    },
+    {
+      "vids": [
+        ],
+        "name": "new circles",
+        "author": "meinaimsuckt",
+        "more": "none",
+        "id": 33,
+        "pass": "Not available",
+        "percentToQualify": 100,
+        "verificationVid": "www.youtube.com/watch?v=B1EKYuNUpjY",
+        "key": 2
+    }
+    ],
+    "version": [
+      1,
+      0,
+      0
+    ]
 };
 
 const list = d.list;const version = d.version;
